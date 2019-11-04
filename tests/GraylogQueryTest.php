1b8c28f09d78a87bffcc5c994beb8a7a4e308956
jspark03 <jspark03@cafe24corp.com>
2019-11-04 08:29:54 +0000

    add test cases

--- tests/GraylogQueryTest.php
+++ tests/GraylogQueryTest.php
@@ -85,6 +85,58 @@ final class GraylogQueryTest extends TestCase
     /**
      * @test
      */
+    public function TC_007_NUMERIC_FIELD()
+    {
+        $query = GraylogQuery::builder()
+            ->field('http_response_code', 500);
+
+        $expect = 'http_response_code:500';
+
+        $this->assertEquals($expect, $query->build());
+    }
+
+    /**
+     * @test
+     */
+    public function TC_008_RANGE_FIELD()
+    {
+        $query = GraylogQuery::builder()
+            ->opField('http_response_code', '>', 500);
+
+        $expect = 'http_response_code:>500';
+
+        $this->assertEquals($expect, $query->build());
+    }
+
+    /**
+     * @test
+     */
+    public function TC_009_FUZZ_FIELD()
+    {
+        $query = GraylogQuery::builder()
+            ->fuzzField('type', 'ssh');
+
+        $expect = 'type:"ssh"~';
+
+        $this->assertEquals($expect, $query->build());
+    }
+
+    /**
+     * @test
+     */
+    public function TC_010_FUZZ_FIELD_WITH_DISTANCE()
+    {
+        $query = GraylogQuery::builder()
+            ->fuzzField('type', 'ssh', 3);
+
+        $expect = 'type:"ssh"~3';
+
+        $this->assertEquals($expect, $query->build());
+    }
+
+    /**
+     * @test
+     */
     public function TC_011_RANGE()
     {
         $query = GraylogQuery::builder()
@@ -111,6 +163,32 @@ final class GraylogQueryTest extends TestCase
     /**
      * @test
      */
+    public function TC_013_RAW()
+    {
+        $query = GraylogQuery::builder()
+            ->raw('/ethernet[0-9]+/');
+
+        $expect = '/ethernet[0-9]+/';
+
+        $this->assertEquals($expect, $query->build());
+    }
+
+    /**
+     * @test
+     */
+    public function TC_014_NOT()
+    {
+        $query = GraylogQuery::builder()
+            ->not()->exists('type');
+
+        $expect = 'NOT _exists_:type';
+
+        $this->assertEquals($expect, $query->build());
+    }
+
+    /**
+     * @test
+     */
     public function TC_015_AND()
     {
         $query = GraylogQuery::builder()
@@ -119,6 +197,44 @@ final class GraylogQueryTest extends TestCase
             ->term('dog');
 
         $expect = '"cat" AND "dog"';
+
+        $this->assertEquals($expect, $query->build());
+    }
+
+    /**
+     * @test
+     */
+    public function TC_016_OR()
+    {
+        $query = GraylogQuery::builder()
+            ->term('cat')
+            ->or()
+            ->term('dog');
+
+        $expect = '"cat" OR "dog"';
+
+        $this->assertEquals($expect, $query->build());
+    }
+
+    /**
+     * @test
+     */
+    public function TC_017_PARENTHESES()
+    {
+        $query = GraylogQuery::builder()
+            ->openParen()
+                ->term('ssh login')
+                ->and()
+                ->openParen()
+                    ->field('source', 'example.org')
+                    ->or()
+                    ->field('source', 'another.example.org')
+                ->closeParen()
+            ->closeParen()
+            ->or()
+            ->exists('always_find_me');
+
+        $expect = '( "ssh login" AND ( source:"example.org" OR source:"another.example.org" ) ) OR _exists_:always_find_me';
 
         $this->assertEquals($expect, $query->build());
     }
